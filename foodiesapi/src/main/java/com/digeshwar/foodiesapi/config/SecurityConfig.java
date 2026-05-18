01acb479cade0e0fc0ee7e7f2e9b3aa871133f95
Digeshwar <mr.digeshwar05@gmail.com>
2026-05-18 11:23:15 +0530

    fix spring security cors

--- foodiesapi/src/main/java/com/digeshwar/foodiesapi/config/SecurityConfig.java
+++ foodiesapi/src/main/java/com/digeshwar/foodiesapi/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
                  .cors(Customizer.withDefaults())
                  .csrf(AbstractHttpConfigurer::disable)
                  .authorizeHttpRequests(auth ->  auth
@@ -58,16 +58,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        return new CorsFilter(corsConfigurationSource());
+//    }
 
-    private UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://admin-online-food-ordering-system.vercel.app", "https://client-online-food-ordering-system.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Origin", "Accept", "X-Requested-With"));
+        config.setAllowedHeaders(List.of("*"));
+//        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Origin", "Accept", "X-Requested-With"));
         config.setExposedHeaders(List.of("Authorization"));
         config.setAllowCredentials(true);
 
